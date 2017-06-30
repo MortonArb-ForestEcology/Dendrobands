3a84a598d29fd2e50891e17e83ee5e8f69b40058
Drew Duckett <duckettdj@g.cofc.edu>
2017-06-30 12:25:26 -0500

    Bug fixes and code condensing
    Dates are now formatted as dates. ggplot code is cleaner. NA removal
    line was deleted because NA removal can be done with read.csv function.

--- dendroband_plotting.R
+++ dendroband_plotting.R
@@ -1,17 +1,16 @@
 library(ggplot2) #Load necessary library
-setwd("") #Set working directory to data file location
-dendroband <- read_csv("") #read in data file
-dendroband$dist_from_collar[which(dendroband$dist_from_collar == "negative")] <- NA #Change observations marked "negative" to NA
+setwd("~/Forest_Ecology/Dendrobands") #Set working directory to data file location
+dendroband <- read.csv("DendrobandObservations_OakCollection_2017-06-27.csv", na.strings=c("", "negative")) #read in data file
+dendroband$date_observed <- as.Date(dendroband$date_observed, format="%m/%d/%Y") #Format dates as dates
 
 
-dendroplot<- ggplot(data=dendroband, aes(x=date_observed, y=as.numeric(dist_from_collar), group = id, col = as.factor(id))) #Set basic plot parameters
-dendroplot<- dendroplot+geom_line(size=.9) #Specify line graph
-dendroplot<- dendroplot+facet_wrap(~species, ncol=3) #Specify panels
-dendroplot<- dendroplot+scale_color_discrete() #Specify color scheme
-dendroplot<- dendroplot+theme(legend.position = "none", axis.text.x = element_text(angle = 90, hjust = 1)) #legend and label parameters
-dendroplot<- dendroplot+scale_y_continuous(breaks=c(0.00, 5.00, 10.00, 15.00, 20.00, 25.00, 30.00)) #Set y axis scale
-dendroplot #Plot graph
 
+ggplot(data=dendroband) + #Set basic plot parameters
+  facet_wrap(~species, ncol=3) + #Specify panels
+  geom_line(aes(x=date_observed, y=dist_from_collar, group = id, col = as.factor(id)), size=.9) + #Specify line graph
+  theme(legend.position = "none", axis.text.x = element_text(angle = 90, hjust = 1)) + #legend and label parameters
+  scale_color_discrete() + #Specify color scheme
+  scale_y_continuous(breaks=c(0.00, 5.00, 10.00, 15.00, 20.00, 25.00, 30.00)) #Set y axis scale
 
 
 
