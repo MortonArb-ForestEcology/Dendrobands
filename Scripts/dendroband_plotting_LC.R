7a0baf06cf3d8d0346d2f2a5161ba72b8d8c5094
Bethany Zumwalde <bzumwalde@mortonarb.org>
2019-07-19 14:23:58 -0500

    Cleaning up script
    Adding comments and descriptions of graphs

--- Scripts/dendroband_plotting_LC.R
+++ Scripts/dendroband_plotting_LC.R
@@ -1,5 +1,7 @@
-library(ggplot2) #Load necessary library
+library(ggplot2) #Load necessary libraries
 library(scales)
+library(dplyr)
+library("ggpubr")
 
 #Loading in the data
 
@@ -8,13 +10,26 @@ setwd("~/GitHub/Dendrobands/Raw_data") #Set working directory to data file locat
 #read in data file
 dendroband <- read.csv("LCDendrometerBands_Quercus_Consolidated_2017-2019.csv", na.strings=c("", "negative", "9999.00", "999.00"))
 dendroband$date_observed <- as.Date(dendroband$date_observed, format="%m/%d/%Y") #Format dates as dates
+
+
+dendroband$taxon <- dendroband$species
+dendroband$taxon <- recode(dendroband$species, alba = "Q. alba", 
+                           bicolor = "Q. bicolor", 
+                           macrocarpa = "Q. macrocarpa",
+                           robur = "Q. robur",
+                           rubra = "Q. rubra",
+                           velutina = "Q. velutina")
+
 summary(dendroband) 
+str(dendroband)
 
 #Order by date observed
 dendroband <- dendroband[order(dendroband$date_observed),] #sort dataframe by date of measurement
 
 #Diameter
 dendroband$dist_from_collar <- dendroband$dist_from_collar / pi #convert circumference to diameter in the dataframe
+
+
 
 #subtract minimum for each tree to offset bands settling
 for(id in unique(dendroband$id)){ #for each tree
@@ -51,7 +66,7 @@ for (id in unique(dendroband$id)){ #for each tree
     
     
   }
- 
+  
   growth <- as.numeric(obs_diff) / as.numeric(date_diff) #Divide mm distance by number of days to get mm/day
   id_df <- do.call(cbind, list(id_df,growth)) #add growth measurements to new column combined with metadata  
   colnames(id_df)[length(id_df)] <- "mm_day" #Change column name
@@ -59,32 +74,123 @@ for (id in unique(dendroband$id)){ #for each tree
   
 }
 
+#2017 data was noisy. Creating subset starting at 2018 just in case
 dendroband$date_observed <- as.POSIXct(dendroband$date_observed, format="%Y-%m-%d")
 after2017 <- subset(dendroband, date_observed > "2018-01-01")
 
 
-#Plot distance
+
+########Plot distance###########
+
+#Plotting total distance by species 2017-2019
+png("~/GitHub/Dendrobands/LC_Figures//LCDendoband_TotalDistanceFromCollar_2017-2019.png")
+ggplot(data=dendroband[!is.na(dendroband$dist_from_collar),]) + #Set basic plot parameters
+  geom_point(aes(x=date_observed, y=dist_from_collar, group = id, col = as.factor(id)), size=1) + #Add points
+  geom_line(aes(x=date_observed, y=dist_from_collar, group = id, col = as.factor(id)), size=.9) + #Specify line graph
+  theme(legend.position = "none",  axis.text.x = element_text(angle = 90, hjust = 1), 
+        plot.title = element_text(hjust=0.5)) + 
+  ylab(expression("Distance from Collar (mm)"))+
+  xlab(expression("Date Observed")) +
+  ggtitle("LC Total Measured Distance from Collar to Line 2017-2019") +
+  scale_x_datetime(date_breaks = "2 months", labels = date_format("%b %Y"))+
+  scale_color_discrete() +
+  facet_wrap(~taxon, ncol=2, scales = "free") #Specify panels
+dev.off()
+
+
+
+#Plotting total distance by species 2018-2019
+png("~/GitHub/Dendrobands/LC_Figures//LCDendoband_TotalDistanceFromCollar_2018-2019.png")
 ggplot(data=after2017[!is.na(after2017$dist_from_collar),]) + #Set basic plot parameters
   geom_point(aes(x=date_observed, y=dist_from_collar, group = id, col = as.factor(id)), size=1) + #Add points
   geom_line(aes(x=date_observed, y=dist_from_collar, group = id, col = as.factor(id)), size=.9) + #Specify line graph
-  theme(legend.position = "none", axis.text.x = element_text(angle = 90, hjust = 1)) + #legend and label parameters
-  scale_x_datetime(date_breaks = "months", labels = date_format("%b %y"))+
+  theme(legend.position = "none",  axis.text.x = element_text(angle = 90, hjust = 1),
+        plot.title = element_text(hjust=0.5)) + #legend and label parameters
+  ylab(expression("Distance from Collar (mm)"))+
+  xlab(expression("Date Observed")) +
+  ggtitle("LC Total Measured Distance from Collar to Line 2018-2019") +
+  scale_x_datetime(date_breaks = "2 months", labels = date_format("%b %Y"))+
   scale_color_discrete() +
-  facet_wrap(~species, ncol=2, scales = "free") #Specify panels
-  
+  facet_wrap(~taxon, ncol=2, scales = "free") #Specify panels
+dev.off()
+
+
+#Boxplot for total distance from collar 2018-2019
+png("~/GitHub/Dendrobands/LC_Figures//LCDendoband_TotalDistancefromCollar_Boxplot_BySpecies.png")
+ggboxplot(after2017, x = "taxon", y = "dist_from_collar", 
+          color = "taxon",
+          legend = "none",
+          ylab = "Distance From Collar (mm)", xlab = "Taxon", 
+          title = "Total Distance from Collar from 2018-2019")+
+  theme(legend.position = "none", plot.title = element_text(hjust=0.5)) #Specify panels
+dev.off()
+
+
+
+
+####################
 
 #Plot growth in mm/day
+
+growth_df1$taxon <- growth_df1$species
+growth_df1$taxon <- recode(growth_df1$species, alba = "Q. alba", 
+                           bicolor = "Q. bicolor", 
+                           macrocarpa = "Q. macrocarpa",
+                           robur = "Q. robur",
+                           rubra = "Q. rubra",
+                           velutina = "Q. velutina")
+
+##subsetting to 2018-2019
 growth_df2 <- na.omit(growth_df1)
 growth_df2$date_observed <- as.POSIXct(growth_df2$date_observed, format="%Y-%m-%d")
 growth_df3 <- subset(growth_df2, date_observed > "2018-01-01")
 
+#Growth per day from 2018-2019
 
+png("~/GitHub/Dendrobands/LC_Figures//LCDendoband_GrowthPerDay2018-2019_Boxplot_Species.png")
+ggboxplot(growth_df3, x = "taxon", y = "mm_day", 
+          color = "taxon",
+          legend = "none",
+          ylab = "Growth per Day (mm)", xlab = "Plot", 
+          title = "Growth per Day from 2018-2019")+
+  theme(legend.position = "none", plot.title = element_text(hjust=0.5)) #Specify panels
+dev.off()
+
+
+#Comparing means of growth per day across species
+tapply(growth_df3$mm_day, growth_df3$taxon, mean)
+tapply(growth_df3$mm_day, growth_df3$taxon, sd)
+res.aov <- aov(mm_day ~ taxon, data = growth_df2)
+summary(res.aov)
+
+#Plotting growth per day from 2017-2019
+png("~/GitHub/Dendrobands/LC_Figures//LCDendoband_GrowthPerDay_2017-2019_BySpecies.png")
+ggplot(data=growth_df2) + #Set basic plot parameters
+  geom_point(aes(x=date_observed, y=mm_day, group = id, col = as.factor(id)), size=1) + #Add points
+  geom_line(aes(x=date_observed, y=mm_day, group = id, col = as.factor(id)), size=.9) + #Specify line graph
+  theme(legend.position = "none",  axis.text.x = element_text(angle = 90, hjust = 1), 
+        plot.title = element_text(hjust=0.5)) + 
+  ylab(expression("Growth (mm)"))+
+  ggtitle("LC Growth per day (mm) 2017-2019") +
+  scale_color_discrete()+ #Specify color scheme
+  scale_x_datetime(date_breaks = "2 months", labels = date_format("%b %Y"))+
+  facet_wrap(~taxon, ncol=2, scales = "free") #Specify panels
+#scale_y_continuous(breaks=c(0.00, 5.00, 10.00)) #Set y axis scale
+dev.off()
+
+
+#Subsetting growth per day from 2018-2019
+png("~/GitHub/Dendrobands/LC_Figures//LCDendoband_GrowthPerDay_2018-2019_BySpecies.png")
 ggplot(data=growth_df3) + #Set basic plot parameters
   geom_point(aes(x=date_observed, y=mm_day, group = id, col = as.factor(id)), size=1) + #Add points
   geom_line(aes(x=date_observed, y=mm_day, group = id, col = as.factor(id)), size=.9) + #Specify line graph
-  theme(legend.position = "none", axis.text.x = element_text(angle = 90, hjust = 1)) + #legend and label parameters
+  theme(legend.position = "none",  axis.text.x = element_text(angle = 90, hjust = 1), 
+        plot.title = element_text(hjust=0.5)) + 
+  ylab(expression("Growth (mm)"))+
+  ggtitle("Growth per day (mm) 2018-2019") +
   scale_color_discrete()+ #Specify color scheme
-  scale_x_datetime(date_breaks = "months", labels = date_format("%b %y"))+
-  facet_wrap(~species, ncol=2, scales = "free") #Specify panels
-  #scale_y_continuous(breaks=c(0.00, 5.00, 10.00)) #Set y axis scale
+  scale_x_datetime(date_breaks = "2 months", labels = date_format("%b %Y"))+
+  facet_wrap(~taxon, ncol=2, scales = "free") #Specify panels
+#scale_y_continuous(breaks=c(0.00, 5.00, 10.00)) #Set y axis scale
+dev.off()
 
